5199e5de452f397c1bf52b5459cee1dc9b2ed07f
Adrian Serbanscu <adrian@adrianserbanescu.com>
2022-03-09 16:53:45 +0100

    closure partial

---
+++ closure.js
@@ -0,0 +1,24 @@
+function range(start, end) {
+  return function publicApi (end) {
+    var array = []
+
+    for (let i = start; i <= end; i++) {
+      array.push(i)
+    }
+
+    console.log(array)
+    return array
+  }
+}
+
+range(3, 3)
+range(3, 8)
+range(3, 0)
+
+var start3 = range(3)
+var start4 = range(4)
+
+start3(3)
+start3(8)
+start3(0)
+start4(6)
