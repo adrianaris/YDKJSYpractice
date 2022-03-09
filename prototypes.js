5b6a1b0130c62cb7b70b71c9ea58483a2aa16198
Adrian Serbanscu <adrian@adrianserbanescu.com>
2022-03-09 17:44:03 +0100

    proto partial

---
+++ prototypes.js
@@ -0,0 +1,46 @@
+function randMax(max) {
+  return Math.trunc(1E9 * Math.random()) % max
+}
+
+var reel = {
+  symbols: [
+    'X', 'Y', 'Z', 'W', '$', '*', '<', '@'
+  ],
+  spin() {
+    if (this.position == null) {
+      this.position = randMax(
+        this.symbols.length - 1
+      )
+    }
+    this.position = (
+      this.position + 100 + randMax(100)
+    ) % this.symbols.length
+  },
+  display() {
+    if (this.position == null) {
+      this.position = randMax(
+        this.symbols.length - 1
+      )
+    }
+    return this.symbols[this.position]
+  } 
+}
+
+var slotMachine = {
+  reels: [
+    Object.create(reel), Object.create(reel), Object.create(reel)
+  ],
+  spin() {
+    this.reels.forEach(function spinReel(reel){
+      reel.spin()
+    })
+  },
+  display() {
+    this.reels.forEach(function displayReel(reel){
+      console.log(reel.display())
+    })
+  }
+}
+
+slotMachine.spin()
+slotMachine.display()
